1c358283ba7f268f624aa8cb00f6dd602ea12379
Anise Lester and Farnell Newton <farnell@>
2019-05-20 20:42:45 -0700

    save tacos and stuff

--- Desktop/JQuery-Project/js/scripts.js
+++ Desktop/JQuery-Project/js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-     $("#walrus-showing").toggle();
-     $("#walrus-hidden").toggle();
+     $("#tacomonster-showing").toggle();
+     $("#tacomonster-hidden").toggle();
    });
  });
+
+$(document).ready(function() {
+$(".blue-thing").click(function() {
+ $("#animal2-showing").toggle();
+  $("#animal2-hidden").toggle();
+ });
+});
